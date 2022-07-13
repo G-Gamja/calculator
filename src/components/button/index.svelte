266a4f9c1ae65a809b57db1b829f18250f49ba5a
G-Gamja <87967564+G-Gamja@users.noreply.github.com>
2022-07-14 01:57:15 +0900

    k

---
+++ src/components/button/index.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+export let text : string;
+// 애로우펑션
+export let onClick : (value: string)=> void;
+export let color: 'orange'| 'grey'| 'dark-grey';
+</script>
+<div class="button" data-color={color} on:click={()=> onClick(text)}>{text}</div>
+<style lang="scss">
+ .button{
+    width: 80px;
+    height: 80px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+
+
+    &[data-color='orange']{
+        background-color: orange;
+    }
+    &[data-color='grey']{
+        background-color: gray;
+    }
+    &[data-color='dark-grey']{
+        background-color: darkgray;
+    }
+ }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {Button} from '@src/components/button'
+import { each, text } from 'svelte/internal';
+let v: string = "sihun";
+const s: number = 10;
+const textNumbers: string[] = 
+['1','2','3','4','5','6','7','8','9','0'];
+</script>
+
+<div>
+    {#each textNumbers as text}
+        <Button {text}  color="orange" onClick={(value)=> alert(value)}/>
+    {/each}
+</div>
+<div>
+    hi
+</div>
+<style lang="scss">
+    .frame{
+        width: 500px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
