612ff1cec4993195abe8c08998089c64b02f5606
G-Gamja <87967564+G-Gamja@users.noreply.github.com>
2022-07-27 03:21:15 +0900

    Update index.svelte

--- src/routes/barchart/index.svelte
+++ src/routes/barchart/index.svelte
@@ -3,9 +3,21 @@
 	import { BarItem } from '@src/components/baritem';
 
 	// const numbers: number[] = [4, 3, 3, 4, 5, 6, 6, 6, 6, 6, 6,7];
-	const numbers: number[] = [4, 3, 1, 2,7];
-	const colorList: string[] = ['red','yellow','pink','blue','black'];
-	 let ulala= [{name: 'mon', val: 4} , {name: 'tue', val: 3} ,{name: 'wed', val: 1} ,{name: 'thr', val: 2},{name: 'fri', val: 7}];
+	//const numbers: number[] = [4, 3, 1, 2, 7];
+	let numbers: any[] = [];
+	let ulala = [
+		{ name: 'mon', val: 4, color: 'purple' },
+		{ name: 'tue', val: 3, color: 'pink' },
+		{ name: 'wed', val: 1, color: 'blue' },
+		{ name: 'thr', val: 2, color: 'yellow' },
+		{ name: 'fri', val: 7, color: 'brown' }
+		,
+		{ name: 'sat', val: 5, color: 'skyblue' }
+	];
+	for (let index = 0; index < ulala.length; index++) {
+		numbers[index] = ulala[index].val;
+		
+	}
 	// let obj1 = { id: 1, name: 'Mark', age: 30, country: "USA" };
 	//get max, min value
 	let maxVal = Math.max(...numbers);
@@ -23,12 +35,17 @@
 	{/each} -->
 
 	{#each ulala as item}
-		<BarItem numberval={item.val} nameVal={item.name} color={barColor} max={maxVal} min={minVal} />
+		<BarItem
+			numberval={item.val}
+			nameVal={item.name}
+			color={item.color}
+			max={maxVal}
+			min={minVal}
+		/>
 	{/each}
 
 	<!-- </span> -->
 </BarChart>
-
 
 <!-- {#each ulala as ulala.keys}
 	
