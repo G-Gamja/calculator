57b2d2c0afd076ccd1870b02f993009c07e932b3
G-Gamja <87967564+G-Gamja@users.noreply.github.com>
2022-08-03 02:29:37 +0900

    라인차트 구현완료
    svg로 라인차트 구현

--- src/components/svgComponents/index.svelte
+++ src/components/svgComponents/index.svelte
@@ -11,68 +11,53 @@
 	// export let heightSive: number;
 </script>
 
-<!-- <div class="root">
-	<div class="title-field">{title}</div>
-	<div class="graph-field">
-		<div class="l-guide">
-			{#each leftGuides as v}
-				<div class="guide-item">{v}</div>
-			{/each}
-		</div>
-		<div class="graphics">
-			<slot />
-		</div>
-	</div>
-	<div class="under-field">// UNDER</div>
-</div> -->
 
 <svg class="graph">
-	<rect width="500" height="800" />
+	<g>
+		<rect x="0" y="0" width="800" height="900" />
+	</g>
+
 	<text x="400" y="20">{title}</text>
-	<g class="grid x-grid" id="xGrid">
-		<line x1="90" x2="90" y1="5" y2="371" />
-	</g>
-	<g class="grid y-grid" id="yGrid">
-		<line x1="90" x2="705" y1="370" y2="370" />
-	</g>
-	<g class="labels x-labels">
-		<text x="100" y="400">2008</text>
-		<text x="246" y="400">2009</text>
-		<text x="392" y="400">2010</text>
-		<text x="538" y="400">2011</text>
-		<text x="684" y="400">2012</text>
-		<text x="400" y="440" class="label-title">{rightLegend}</text>
-	</g>
-	<g class="labels y-labels">
-		<text x="80" y="15">15</text>
-		<text x="80" y="131">10</text>
-		<text x="80" y="248">5</text>
-		<text x="80" y="373">0</text>
-		<text x="50" y="200" class="label-title">{leftLegend}</text>
-	</g>
-	<g class="data" data-setname="Our first data set">
-		<circle cx="90" cy="192" data-value="7.2" r="4" />
-		<circle cx="240" cy="141" data-value="8.1" r="4" />
-		<circle cx="388" cy="179" data-value="7.7" r="4" />
-		<circle cx="531" cy="200" data-value="6.8" r="4" />
-		<circle cx="677" cy="104" data-value="6.7" r="4" />
-	</g>
-	<g class="linedata">
-		<line x1="90" x2="240" y1="192" y2="141" />
-		<line x1="90" x2="90" y1="5" y2="371" />
-		<line x1="90" x2="90" y1="5" y2="371" />
-		<line x1="90" x2="90" y1="5" y2="371" />
+	<g class="graph_back">
+		<g class="grid x-grid" id="xGrid">
+			<line x1="90" x2="90" y1="10" y2="370" />
+		</g>
+		<g class="grid y-grid" id="yGrid">
+			<line x1="90" x2="700" y1="370" y2="370" />
+		</g>
+		<g class="labels x-labels">
+
+			<text x="400" y="440" class="label-title">{rightLegend}</text>
+		</g>
+		<g class="labels y-labels">
+			<!-- <text x="80" y="15">15</text>
+			<text x="80" y="131">10</text>
+			<text x="80" y="248">5</text>
+			<text x="80" y="370">0</text> -->
+			<text x="50" y="200" class="label-title">{leftLegend}</text>
+		</g>
+
+		<g class="linedata">
+
+			<slot />
+		</g>
 	</g>
 </svg>
 
 <style lang="scss">
+	$graph_height: 500px;
+	$graph_width: 800px;
 	rect {
 		fill: pink;
 		stroke-width: 2px;
 		stroke: rgb(0, 0, 0);
 	}
-	.linedata{
-		stroke: blue;
+	// .linedata {
+	// 	stroke: blue;
+	// }
+	.graph_back {
+		background-color: #f00;
+		margin-top: 20px;
 	}
 	.graph .labels.x-labels {
 		text-anchor: middle;
@@ -84,14 +69,15 @@
 
 	.graph {
 		margin-top: 100px;
-		height: 500px;
-		width: 800px;
+		height: $graph_height;
+		width: $graph_width;
 	}
 
 	.graph .grid {
 		stroke: black;
 		stroke-dasharray: 0;
 		stroke-width: 3;
+		margin-top: 40px;
 	}
 
 	.labels {
@@ -105,13 +91,6 @@
 		fill: black;
 	}
 
-	.data {
-		fill: red;
-		stroke-width: 1;
-	}
-	.linedata{
-
-	}
 	.root {
 		$title-height: 48px;
 		$under-height: 32px;
